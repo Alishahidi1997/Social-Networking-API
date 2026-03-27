202db4e0ea37a90e1ea5234b4b651f8603e6bbbd
Ali Shahidi <shahidi.ali.1997@gmail.com>
2026-03-27 08:50:25 -0600

    fixed  accountservice error

--- Services/IAccountService.cs
+++ Services/IAccountService.cs
@@ -4,7 +4,7 @@ namespace API.Services;
 
 public interface IAccountService
 {
-    Task<(UserDto? User, string? Token)?> RegisterAsync1(RegisterDto dto, CancellationToken ct = default);
+    Task<(UserDto? User, string? Token)?> RegisterAsync(RegisterDto dto, CancellationToken ct = default);
     Task<(UserDto? User, string? Token)?> LoginAsync(LoginDto dto, CancellationToken ct = default);
     Task<bool> DeleteAccountAsync(int userId, CancellationToken ct = default);
 }
